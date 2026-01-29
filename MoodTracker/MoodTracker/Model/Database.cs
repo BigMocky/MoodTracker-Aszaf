e0c17dd5dcc6d1ce2dfc8b423a0a64fdf25f91b5
denesmor <denesmorgotli@gmail.com>
2026-01-29 12:30:32 +0100

    Database class kód, program class függvények és kód

--- MoodTracker/MoodTracker/Model/Database.cs
+++ MoodTracker/MoodTracker/Model/Database.cs
@@ -8,5 +8,27 @@ namespace MoodTracker.Model
 {
     internal class Database
     {
+        private int _Mood;
+        private double _Atlag;
+        private string _Datum;
+        private string _Motivation;
+
+        public Database()
+        {
+
+        }
+
+        public Database(int mood, double atlag, string datum, string motivation)
+        {
+            Mood = mood;
+            Atlag = atlag;
+            Datum = datum;
+            Motivation = motivation;
+        }
+
+        public int Mood { get => _Mood; set => _Mood = value; }
+        public double Atlag { get => _Atlag; set => _Atlag = value; }
+        public string Datum { get => _Datum; set => _Datum = value; }
+        public string Motivation { get => _Motivation; set => _Motivation = value; }
     }
 }
--- MoodTracker/MoodTracker/Program.cs
+++ MoodTracker/MoodTracker/Program.cs
@@ -1,7 +1,61 @@
-﻿internal class Program
+﻿using MoodTracker.Model;
+using System.Diagnostics;
+using System.Security.AccessControl;
+
+internal class Program
 {
+    private static List<List<string>> moodok = new List<List<string>>();
+    private static List<Database> database = new List<Database>();
+    private static FileRead.ReadFromFile reader = new FileRead.ReadFromFile();
+//    private static FileRead.WriteToFile writer = new FileRead.WriteToFile();
+
     private static void Main(string[] args)
     {
-        
+        AdatBeolvasas("moodtracker.sql", 4, ';', false);
+        AdatBetoltes(moodok);
+        AdatDatabasebeIras();
+    }
+
+    private static void AdatDatabasebeIras()
+    {
+        /*
+               List<string> fileData = new List<string>();
+               foreach (var item in moodok)
+               {
+                   if (item.Mood.Substring(0, 1).ToLower() == "a")
+                   {
+                       fileData.Add($"{item.Mood};{item.Datum}");
+                   }
+               }
+
+               try
+               {
+                   writer.FileWrite("moodtracker.sql", fileData);
+               }
+               catch (Exception)
+               {
+
+                   throw;
+               }
+        */
+    }
+
+    private static void AdatBetoltes(List<List<string>> moodok)
+    {
+        foreach (var item in moodok)
+        {
+            Database m = new Database();
+            m.Mood = Convert.ToInt32(item[0]);
+            m.Atlag = Convert.ToDouble(item[1]);
+            m.Datum = item[2];
+            m.Motivation = item[3];
+
+            database.Add(m);
+        }
+    }
+
+    private static void AdatBeolvasas(string v1, int v2, char v3, bool v4)
+    {
+        moodok = reader.FileRead(v1, v2, v3, v4);
     }
 }
